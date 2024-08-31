a5a5e554a8b215b5b2d6894e5c739f9871a58041
shadab <shad46243@gmail.com>
2024-09-01 04:57:21 +0530

    vite project set up

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import HeroHome from './Components/HeroHome'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <HeroHome/>
+      
+    </>
+  )
+}
+
+export default App
