2d95de46b197a22c1981c48fffa2d15964ab47d1
navneetshahi14 <navneet.shahi2004@gmail.com>
2024-09-03 21:51:40 +0530

    updated admin login and register pages

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Inputs from '../ui/Inputs';
 import google from '../../assets/Google.png'
 import Authcontext from '../../Context/Authcontext';
@@ -7,12 +7,29 @@ import { useNavigate } from 'react-router-dom';
 const Login = () => {   
 
     const {isnotLogged,setLogged} = useContext(Authcontext)
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [username,setUsername] = useState('')
+    const [confirmPassword,setConfirmPassword] = useState("")
+    const [role,setRole] = useState('')
+    const [wrong,setWrong] = useState("border-none")
+    const [instituteId,setInstitueId] = useState("")
+    const [department,setDepartment] = useState("")
 
-
+    const handleRegister =()=>{
+        if(confirmPassword=== password){
+            // Register api call
+        }else{
+            setWrong("border animate-shake animate-twice animate-duration-1000")
+            setTimeout(() => {
+                setWrong('border-none ')
+            }, 1000);
+        }
+    }
 
     return (
         <>
-        <div className={`w-[70%] relative h-[80%] bg-white ${isnotLogged?"rounded-r-xl":"rounded-l-xl"} flex`}>
+        <div className={`w-[70%]  relative  min-h-[80%] max-h-[90%] bg-white ${isnotLogged?"rounded-r-xl":"rounded-l-xl"} flex`}>
             <div className=' flex flex-col justify-center items-center gap-5 rounded-xl bg-white w-[50%] py-12'>
                 <h1 className="text-3xl font-bold">SignIn</h1>
                 <div className="flex gap-2 justify-center items-center border-gray-500 border cursor-pointer p-1 rounded">
@@ -20,8 +37,13 @@ const Login = () => {
                     <span>SignIn with Google</span>
                 </div>
                 <p className="text-sm">Or</p>
-                <Inputs id={"email"} pholder='Email'/>
-                <Inputs id='password' pholder='Password' />
+                <Inputs value={email} Changes={(e)=>setEmail(e.target.value)} id={"email"} pholder='Email'/>
+                <Inputs value={password} Changes={(e)=>setPassword(e.target.value)} id='password' pholder='Password' />
+                <select name="" id="" value={role} onChange={(e)=>setRole(e.target.value)} className='w-[70%] p-2 rounded-lg shadow-md' >
+                    <option value="">Your Role</option>
+                    <option value="admin">Admin</option>
+                    <option value="faculty">Faculty</option>
+                </select>
                 <p className=''>Forget Password?</p>
                 <button className='bg-primary p-2 rounded text-white px-10 font-bold'>SIGN IN</button>
 
@@ -39,11 +61,18 @@ const Login = () => {
                     <span>SignIn with Google</span>
                 </div>
                 <p className="text-sm">Or</p>
-                <Inputs id={"User"} pholder='UserName'/>
-                <Inputs id={"email"} pholder='Email'/>
-                <Inputs id={"password"} pholder='Password'/>
-                <Inputs id={"email"} pholder='Email'/>
-                <button className='bg-primary p-2 rounded text-white px-10 font-bold'>SIGN IN</button>
+                <Inputs id={"User"} value={username} Changes={(e)=>setUsername(e.target.value)} pholder='UserName'/>
+                <Inputs id={"email"} value={email} Changes={(e)=>setEmail(e.target.value)} pholder='Email'/>
+                <Inputs id={"InstituteId"} value={instituteId} Changes={(e)=>setInstitueId(e.target.value)} pholder='Institute Id'/>
+                <Inputs id={"Department"} value={department} Changes={(e)=>setDepartment(e.target.value)} pholder='Department'/>
+                <Inputs id={"password"} value={password} Changes={(e)=>setPassword(e.target.value)} pholder='Password'/>
+                <Inputs id={"confirmPassword"} wrong={wrong} value={confirmPassword} Changes={(e)=>setConfirmPassword(e.target.value)} pholder='Confirm Password'/>
+                <select name="" id="" value={role} onChange={(e)=>setRole(e.target.value)} className='w-[70%] p-2 rounded-lg shadow-md'>
+                    <option value="">Your Role</option>
+                    <option value="admin">Admin</option>
+                    <option value="faculty">Faculty</option>
+                </select>
+                <button onClick={handleRegister} className='bg-primary border-red-500 p-2 rounded text-white px-10 font-bold'>SIGN IN</button>
 
             </div>
         </div>
