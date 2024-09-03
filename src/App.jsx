5668a9c2d6656ecacadfc827c0c58027aa071e57
jsscoder <you@example.com>
2024-09-03 21:16:34 +0530

    changes to dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,29 @@
 import { useState } from 'react'
 
-import { BrowserRouter, Route ,Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { LandingPage } from './pages/LandingPage/Landing'
 import Login from './Components/Login/Login'
 import Overlayer from './pages/Overlay/Overlayer'
+import AdminDashboard from './Components/Dashboard/AdminDashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+
       <BrowserRouter>
         <Routes>
+        <Route path="/login/adminDashboard" element={<AdminDashboard />} />
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<Overlayer/>} />
-          
+          <Route path="/login" element={<Overlayer />} />
+
+
+   
         </Routes>
       </BrowserRouter>
-    
-      
+
+
     </>
   )
 }
